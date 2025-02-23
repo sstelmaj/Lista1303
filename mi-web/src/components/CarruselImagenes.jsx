ed94a2ac1a52a5f0af5b0a69f71cc369f42ac005
sstelmaj <seba.stelmaj21@gmail.com>
2025-02-23 13:44:42 -0300

    mejoras en caracteristicas del carrusel

--- mi-web/src/components/CarruselImagenes.jsx
+++ mi-web/src/components/CarruselImagenes.jsx
@@ -13,10 +13,14 @@ import imagen6 from "../assets/imagen6.jpeg";
 const images = [imagen1, imagen2, imagen3, imagen4, imagen5, imagen6];
 
 export default function SidebarCarousel() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [isDragging, setIsDragging] = useState(false); // Para detectar el deslizamiento
+
   var settings = {
     dots: true,
-    infinite: true,
-    speed: 1500,
+    infinite: false,
+    speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
     initialSlide: 0,
@@ -46,17 +50,61 @@ export default function SidebarCarousel() {
         }
       }
     ],
+    beforeChange: () => {
+      setIsDragging(true); // El usuario está comenzando a deslizar
+    },
+    afterChange: () => {
+      setIsDragging(false); // El deslizamiento ha terminado
+    }
+  };
+
+  // Función para manejar el clic en la imagen
+  const handleImageClick = (image) => {
+    if (!isDragging) { // Solo abre el modal si no se está deslizando
+      setSelectedImage(image);
+      setIsModalOpen(true);
+    }
+  };
+
+  // Función para cerrar el modal
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
   return (
-    <div className="slider-container w-400 mx-auto mt-100">
+    <div className="slider-container w-300 mx-auto mt-10">
       <Slider {...settings}>
         {images.map((image, index) => (
-          <div key={index}>
-            <img src={image} alt={`carousel-${index}`} className="w-125 h-70 rounded-lg object-cover border-4 border-black" />
+          <div key={index} className="px-2">
+            <img
+              src={image}
+              alt={`carousel-${index}`}
+              className="w-full h-70 rounded-lg object-cover border-4 border-black cursor-pointer"
+              onClick={() => handleImageClick(image)} // Solo abre el modal si no se está deslizando
+            />
           </div>
         ))}
       </Slider>
+
+      {/* Modal */}
+      {isModalOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+          <div className="bg-white p-4 rounded-lg relative">
+            {/* Botón de cierre con fondo blanco circular */}
+            <button
+              className="absolute top-4 right-4 text-black text-4xl font-bold bg-white rounded-full p-2 shadow-md border-4 border-black cursor-pointer"
+              onClick={closeModal}
+            >
+              X
+            </button>
+            <img
+              src={selectedImage}
+              alt="full-size"
+              className="max-w-400 max-h-200 rounded-lg"
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
