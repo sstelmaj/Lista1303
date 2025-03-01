008099c148a3d8b43f1b3e6303c9b08d8e4a0a0b
sstelmaj <seba.stelmaj21@gmail.com>
2025-03-01 18:08:13 -0300

    agregando .env para el deploy

--- mi-web/tailwind.config.cjs
+++ mi-web/tailwind.config.cjs
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"], // Asegúrate de que apunta a tus archivos HTML y JS
+  content: ["./src/**/*.{html,js}"], 
   theme: {
     extend: {},
   },
   plugins: [],
+  experimental: {
+    disableRust: true
+  },
 };
